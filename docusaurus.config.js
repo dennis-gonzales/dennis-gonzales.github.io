fda7968fe43bce9efef7e90a81402ec9a5b066b1
Dennis Gonzales <pro.gonzalesdennis@gmail.com>
2023-07-02 22:15:16 +0800

    chore: update title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'My Learning Paths',
+  tagline: 'Ah yes, finally a documentation of what I have learned.',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -72,9 +72,9 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'My Learning Paths',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'My Learning Paths Logo',
           src: 'img/logo.svg',
         },
         items: [
