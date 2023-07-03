70eedc7d9b6ff508a60647243f265847ef02d3c1
Dennis Gonzales <pro.gonzalesdennis@gmail.com>
2023-07-03 11:00:13 +0800

    build: ignore broken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,7 @@ const config = {
   organizationName: 'dennis-gonzales', // Usually your GitHub org/user name.
   projectName: 'dennis-gonzales.github.io', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful
